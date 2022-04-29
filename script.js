d325f0ac01daea8de784bea88af696fc0fdb86ce
hdyehy <trigo.matthew@outlook.com>
2022-04-29 12:52:30 -0500

    added an explanation to my thought process so I didnt look like I was cheating, wanted to show my work

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ console.log(arrowAdd100(160)); // returns 260
 
 //problem #3
 const arrowCharacterCount = (str , chr) => {
-    const answer = (str.split(chr).length - 1) 
+    const answer = (str.split(chr).length - 1);
+    // to explain what I did here, split splits up a string into an array. inside split, is called the seperator. The seperator tells split where to seperate the string into new array values. when empty, split seperates each character into a seperate index of the array. what I did was tell it to add a split every time it encounters the chr (character) value. in the below case it was 'l'. it then measures that array - 1 (to take into account the other characters in the array) and that is the answer!
 return answer }
 
-    console.log(arrowCharacterCount('hellllo','l')); //returns 4
+    console.log(arrowCharacterCount('heldldlgalo','l')); //returns 4
